7c30ab33de074a95f4b21b77445c9fdbc854f7ae
Andre Rigotto <alrigotto@yahoo.com.br>
2025-01-23 22:10:33 -0300

    Starting SQLite implementation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Flet and SQLite CRUD testing
+# Flet and SQLite CRUD
+
+## Based on YouTube video tutorial in channel: [@setprogramacao](https://www.youtube.com/watch?v=3MIVXEo_NtA)
+
+### It's just only a CRUD testing using
+
+#### - [Flet Python](https://flet.dev/) framework
+
+#### - [SQLite](https://www.sqlite.org/)
+
+#### - [UV](https://github.com/astral-sh/uv) Python packaging
--- flet_crud.py
+++ flet_crud.py
@@ -1,28 +1,130 @@
 import flet as ft
+import sqlite3
+
+
+# database connection
+
+connection = sqlite3.connect("data.db", check_same_thread=False)
+cursor = connection.cursor()
+
+# create table in database
+
+def create_table():
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS costumers (id INTEGER PRIMARY KEY
+        AUTOINCREMENT, name TEXT)
+        
+        """
+    )
+    
+create_table()   
 
 def main(page: ft.Page):
-    page.title = "Flet counter example - Andre"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.CENTER, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.END,
+    page.title = "CRUD COM SQLITE"
+    page.window.height=600
+    page.window.width=300
+    page.window.min_width=300
+    # page.horizontal_alignment = "CENTER"
+    # page.vertical_alignment = "CENTER"
+    
+    # def on_resized(e):
+    #     page.add(ft.Text(f"Janela redimensionada para {e.width}x{e.height}"))
+    # page.on_resized = on_resized
+    
+    def add_data(e):
+        cursor.execute(
+            "INSERT INTO costumers (name) VALUES (?)",
+            [field_add_data.content.controls[0].value]
         )
+        connection.commit()
+        
+    def show_data(e):
+        cursor.execute("SELECT * FROM costumers")
+        rows = cursor.fetchall()
+        # print(rows)
+        for row in rows:
+            page.add(ft.Text(f"ID: {row[0]}, Name: {row[1]}"))
+        page.update()
+    
+    # def show_data(e):
+    #     page.add(ft.Text(field_add_data.content.controls[0].value))
+    #     field_add_data.content.controls[0].value = ""
+    #     page.update()
+    
+    main_title = ft.Container(
+        content=ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.Icon(
+                    name=ft.Icons.FORMAT_LIST_NUMBERED_RTL,
+                    color=ft.Colors.BLUE_300,
+                ),
+                ft.Text(
+                    value="CRUD COM SQLITE",
+                    size=16,
+                    color=ft.Colors.BLUE_300,
+                    # bgcolor=ft.Colors.WHITE12,
+                    weight=ft.FontWeight.W_500,
+                ),    
+            ],
+        ),
+        margin=5,
+        padding=5,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.WHITE12,
+        width=page.on_resized,
+        height=40,
+        border_radius=10,
     )
+    
+    field_add_data = ft.Container(
+        content=ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                ft.TextField(
+                    label="Enter data:",
+                    height=35,
+                    width=200,
+                    text_size=15,
+                    border_radius=10,
+                    border_color=ft.Colors.TRANSPARENT,
+                    color=ft.Colors.BLUE_300,
+                    label_style=ft.TextStyle(size=15, color=ft.Colors.WHITE24),
+                    
+                    # hint_text="dsdssdd",
+                    # hover_color=ft.Colors.RED,
+                    # bgcolor=ft.Colors.WHITE12,
+                ),
+                ft.IconButton(
+                    icon=ft.Icons.CHECK_CIRCLE_OUTLINE_ROUNDED,
+                    icon_color=ft.Colors.BLUE_300,
+                    on_click=add_data,
+                    
+                ),
+            ],
+        ),
+        margin=5,
+        padding=5,
+        alignment=ft.alignment.center,
+        bgcolor=ft.Colors.WHITE12,
+        width=page.on_resized,
+        height=50,
+        border_radius=10,
+    )
+    
+    show_button = ft.Button(text="Show data", on_click=show_data)
+    
+    page.add(
+        # teste,
+        # teste_txt,
+        main_title,
+        field_add_data,
+        show_button,
+    )
+    
+    
+    page.update()
+
+
 
 ft.app(main)
